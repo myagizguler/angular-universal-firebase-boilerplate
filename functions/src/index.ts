b308b2b144eb3f5a00f4ed6b0d3cd6da71259ed4
Ribal <ribal.nasr@gmail.com>
2019-01-16 19:48:55 +0200

    Added Lazy Loading support.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,14 @@
-import * as angularUniversal from 'angular7-universal-express-firebase';
+import * as angularUniversal from './express/express-firebase';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+const { AppModuleNgFactory, LAZY_MODULE_MAP } = require('./server/main');
 
-export const server = angularUniversal.trigger({
+export const server1 = angularUniversal.trigger({
   index: __dirname + '/server/index.html',
   main: __dirname + '/server/main',
   enableProdMode: true,
   cdnCacheExpiry: 1200,
   browserCacheExpiry: 600,
+  extraProviders: [
+    provideModuleMap(LAZY_MODULE_MAP)
+  ]
 });
