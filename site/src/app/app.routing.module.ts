919ff69a0ad4003830aabd4507d8158877936729
Ribal <ribal.nasr@gmail.com>
2019-05-15 16:43:27 +0300

    Fixed an issue preventing fire:serve to run correctly.

--- site/src/app/app.routing.module.ts
+++ site/src/app/app.routing.module.ts
@@ -1,12 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { environment } from '../environments/environment';
 
-const routes: Routes = [
+let routes: Routes = [
   {
     path: '',
-    loadChildren: './pages/home/home.module#HomeModule'
-  }
+    children: [
+      {
+        path: '',
+        loadChildren: './pages/home/home.module#HomeModule'
+      }
+    ]
+  },
 ];
+
+if (environment.production) {
+  // Required to run fire:serve locally.
+  routes = [
+    {
+      path: '',
+      children: routes
+    },
+    {
+      path: ':projectId/us-central1/ssr',
+      redirectTo: '',
+      pathMatch: 'full'
+    },
+    {
+      path: ':projectId/us-central1/ssr/:route',
+      redirectTo: ':route',
+      pathMatch: 'full'
+    },
+  ];
+}
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
